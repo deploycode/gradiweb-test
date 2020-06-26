690c59a5422496084bfc20e528cdd40137f7c67f
Gabriel Rodríguez <gegrodriguez@indracompnay.com>
2020-06-26 16:14:49 -0500

    Uploading files

--- app/Product.php
+++ app/Product.php
@@ -8,4 +8,13 @@ class Product extends Model
 {
     protected $table = 'products';
     protected $fillable = ['name','description','photo','price'];
+
+    public function setPhotoAttribute($photo){
+        if (!empty($photo)) {
+            $new_name= $this->attributes['name'].$photo->getClientOriginalName();
+            $this->attributes['photo']= $new_name;
+            \Storage::disk('public')->put($new_name,\File::get($photo));
+        }
+    }
+
 }
