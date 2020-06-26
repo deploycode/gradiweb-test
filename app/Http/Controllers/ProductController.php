1af6d90b56fa96527fcc5f1bfd35bec4cf7fb6dc
Gabriel Rodríguez <gegrodriguez@indracompnay.com>
2020-06-26 11:13:36 -0500

    Defining layouts

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,7 +13,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return view('home');
     }
 
     /**
@@ -23,7 +23,7 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        return view('dashboard');
     }
 
     /**
