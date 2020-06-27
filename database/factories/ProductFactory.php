804f8dd8fc089296948ee4b781ad46425c641a25
Gabriel Rodríguez <gegrodriguez@indracompnay.com>
2020-06-27 16:22:20 -0500

    Catching exception to Apache Servers

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -6,10 +6,20 @@ use App\Product;
 use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
-    return [
-        'name' => $faker->word,
-        'description' => $faker->paragraph,
-        'photo' => $faker->image($dir = '/tmp', $width = 640, $height = 480),
-        'price' => $faker->numberBetween($min = 1000, $max = 9000),
-    ];
+
+    try {
+        return [
+            'name' => $faker->word,
+            'description' => $faker->paragraph,
+            'photo' => $faker->image($dir = '/tmp', $width = 640, $height = 480),
+            'price' => $faker->numberBetween($min = 1000, $max = 9000),
+        ];
+    } catch (Exception $e) {
+        return [
+            'name' => $faker->word,
+            'description' => $faker->paragraph,
+            'price' => $faker->numberBetween($min = 1000, $max = 9000),
+        ];
+    }
+
 });
